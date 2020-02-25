dc10845883e4dc45e1277c2e894aaac0c9a00940
yy2905 <60762215+yy2905@users.noreply.github.com>
2020-02-25 15:58:36 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,131 @@
+// this is my mapboxGL token
+// the base style includes data provided by mapbox, this links the requests to my account
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nLXFyaSIsImEiOiJjazZncWRkZGowb3kyM25vZXkwbms2cW0xIn0.lbwola6y7YDdaKLMdjif1g';
+
+// set central points and z-level 
+var initialCenterPoint = [86.0679563, 38.1253591]
+var initialZoom = 3.85
+
+// a helper function for Lookup for looking up all the provinces and their status quo
+var nameLookup = (code) => {
+  switch (code) {
+    case 1:
+      return {
+        color: '#8B0000', 
+        description: 'Hubei',
+      };
+    case 2:
+      return {
+        color: '#CD3700',
+        description: 'Henan, confirmed cases: 1,271; cure cases: 822',
+      };
+   }
+};
+
+var defaultText = '<p>Get more inormation about </p>'
+$('#feature-info').html(defaultText)
+
+var initOptions = {
+  container: 'map-container', // put the map in this container
+  style: 'mapbox://styles/mapbox/dark-v10', // use this basemap
+  center: initialCenterPoint, // initial view center
+  zoom: initialZoom, // initial view zoom level (0-18)
+  
+  var map = new mapboxgl.Map(initOptions);
+
+// add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+// wait for the initial style to Load
+map.on('style.load', function() {
+
+  // add a geojson source to the map using our external geojson file
+  map.addSource('pluto-bk-cd6', {
+    type: 'geojson',
+    data: './data/brooklyn_cd_6_trimmed.geojson',
+  });
+
+  // let's make sure the source got added by logging the current map state to the console
+  console.log(map.getStyle().sources)
+
+  // add a layer for our custom source
+  map.addLayer({
+    id: 'fill-map-province',
+    type: 'fill',
+    source: 'NHFPC',
+    paint: {
+      'fill-color': {
+        type: 'categorical',
+        property: 'name',
+        stops: [
+          [
+            'Hubei',
+            nameLookup(1).color,
+          ],
+          [
+            'Henan',
+            nameLookup(2).color,
+          ],
+        ]
+      }
+    }
+  })
+  
+  map.addSource('highlight-feature', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  })
+  
+  map.addLayer({
+    id: 'highlight-line',
+    type: 'line',
+    source: 'highlight-feature',
+    paint: {
+      'line-width': 2,
+      'line-opacity': 0.9,
+      'line-color': 'white',
+        }
+  });
+  
+  // listen for the mouse moving over the map and react when the cursor is over our data
+
+  map.on('mousemove', function (e) {
+    // query for the features under the mouse, but only in the lots layer
+    var features = map.queryRenderedFeatures(e.point, {
+        layers: ['fill-map-province'],
+    });
+
+    // if the mouse pointer is over a feature on our layer of interest
+    // take the data for that feature and display it in the sidebar
+    if (features.length > 0) {
+      map.getCanvas().style.cursor = 'pointer';  // make the cursor a pointer
+
+      var hoveredFeature = features[0]
+      var featureInfo = `
+        <h4>${hoveredFeature.properties.Address}</h4>
+        <p><strong>Province:</strong> ${nameLookup(parseInt(hoveredFeature.properties.name)).description}</p>
+        // <p><strong>Zoning:</strong> ${hoveredFeature.properties.ZoneDist1}</p>
+      `
+      $('#feature-info').html(featureInfo)
+
+      // set this lot's polygon feature as the data for the highlight source
+      map.getSource('highlight-feature').setData(hoveredFeature.geometry);
+    } else {
+      // if there is no feature under the mouse, reset things:
+      map.getCanvas().style.cursor = 'default'; // make the cursor default
+
+      // reset the highlight source to an empty featurecollection
+      map.getSource('highlight-feature').setData({
+        type: 'FeatureCollection',
+        features: []
+      });
+
+      // reset the default message
+      $('#feature-info').html(defaultText)
+    }
+  })
+
+})
